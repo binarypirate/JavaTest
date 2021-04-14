e2c74dd1f347dcbf03e95595047ef0ae6b2e5d19
electrodragon <attaullahkhanlodhi@gmail.com>
2021-04-14 18:01:43 +0500

    Add more tasks

---
+++ src/com/company/sections/section8_method_overloading/FunTask1.java
@@ -0,0 +1,28 @@
+package com.company.sections.section8_method_overloading;
+
+public class FunTask1 {
+    /**********************************|
+    |            quotient              |
+    |            ________________      |
+    |   divisor | dividend             |
+    |           |                      |
+    |           |-----------------     |
+    |            remainder             |
+    |**********************************/
+
+    public static void main(String[] args) {
+        // Note: You have to use same name for each function.
+        // You have to call functions from functions.
+    }
+
+    // todo: Create a function which takes 1 short divisor and 1 long dividend
+    // and return true or false if dividend is completely divisible by divisor.
+
+    // todo: create a function which takes a short[] array_of_dividends & 1 byte of divisor.
+    // function should loop through all dividends & print if dividend is completely
+    // divisible by divisor.
+
+    // todo: create a function which takes a short VarArgs of dividends & 1 byte of divisor.
+    // function should loop through all dividends & print if dividend is completely
+    // divisible by divisor.
+}
