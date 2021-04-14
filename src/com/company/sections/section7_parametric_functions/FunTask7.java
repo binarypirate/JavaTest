7da52d25e3cb56f9586fba21138dea92de2a8bdc
electrodragon <attaullahkhanlodhi@gmail.com>
2021-04-14 17:12:49 +0500

    Add More Tasks

---
+++ src/com/company/sections/section7_parametric_functions/FunTask7.java
@@ -0,0 +1,20 @@
+package com.company.sections.section7_parametric_functions;
+
+/*
+ ` Create a function which takes byte VarArgs
+ ` Then you have to return an integer array
+ ` Containing only odd numbers from that byte VarArgs.
+ ` if we call fun(1,2,3,4,5,6,7,8,9,10,11,12) with these values
+ ` The function will return an int array of odd numbers in it
+ ` { 1, 3, 5, 7, 9, 11 }
+ */
+public class FunTask7 {
+
+    public static void main(String[] args) {
+        // todo: Call That function & then store its result,
+        // todo: Then loop on that int array, and print all values
+    }
+
+    // todo: Create a function which takes byte VarArgs & Returns all odd numbers of VarArgs as int array.
+
+}
