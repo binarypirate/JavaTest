2bbb96886c0dcba933904d7a45a47baf6e701503
electrodragon <attaullahkhanlodhi@gmail.com>
2021-04-14 17:32:45 +0500

    Add More Tasks

---
+++ src/com/company/sections/section7_parametric_functions/FunTask8.java
@@ -0,0 +1,46 @@
+package com.company.sections.section7_parametric_functions;
+
+public class FunTask8 {
+    String[][] USERS = {
+            {"attaullahkhanlodhi@linux.com", "+92-3XX-1234567"},
+            {"youmadbro@madness.com", "+1-3XX-1234567"},
+            {"lovebirds@peak.com", "+311-3XX-1234567"},
+    };
+
+    public static void main(String[] args) {
+        // todo: You have given a 2-D array of USERS.
+        // Each Sub-Array contains an email and a PhoneNumber
+        // You have to loop through all
+        // And print 3 things of each user (domain, username and country-code),
+        // Domain: linux.com
+        // Username: lovebirds
+        // CountryCode: +92
+    }
+
+    /*
+     * This function will take an email, and should return username
+     * string before @ symbol is called username.
+     */
+    private static String getUsernameFromEmail(String email) {
+        // todo: Complete this function so it should return username
+        return null;
+    }
+
+    /*
+     * This function will take an email, and should return domain
+     * string after @ symbol is called domain. (gmail.com)
+     */
+    private static String getDomainFromEmail(String email) {
+        // todo: Complete this function so it should return domain
+        return null;
+    }
+
+    /*
+     * This function will take a PhoneNumber, and should return Country Code
+     * (+92, +1, +91) these are called country codes.
+     */
+    private static String getCountryCodeFromPhone(String phoneNumber) {
+        // todo: Complete this function so it should return country code
+        return null;
+    }
+}
